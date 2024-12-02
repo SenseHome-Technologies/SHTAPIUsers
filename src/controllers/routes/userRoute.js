cab0073f4d43a9f044f6119b426a57626284e857
TinoMeister <tiagooliveira869@gmail.com>
2024-12-02 12:08:04 +0000

    Update swagger

--- src/controllers/routes/userRoute.js
+++ src/controllers/routes/userRoute.js
@@ -20,6 +20,9 @@ const router = require('express').Router();
  * @apiError {String} message Invalid credentials
  * @apiError {String} message User not found
  * @apiError {String} message Error logging in
+ *
+ * @apiDescription This endpoint allows a user to log in by providing their email and password. If the credentials are valid,
+ * a JWT token is returned to the user.
  */
 router.route('/user/login').post(
     // Define an asynchronous function to handle the login route
@@ -42,17 +45,21 @@ router.route('/user/login').post(
 )
 
 /**
- * @api {post} /user/register Register
- * @apiName Register
+ * @api {post} /user/register Register User
+ * @apiName Register User
  * @apiGroup User
  * @apiParam {String} username Username of the user
  * @apiParam {String} password Password of the user
  * @apiParam {String} email Email of the user
  * @apiSuccess {String} message User registered successfully
- * @apiError {String} message Username or password is missing
- * @apiError {String} message Username already exists
- * @apiError {String} message Password must be at least 8 characters long
- * @apiError {String} message Error creating user
+ * @apiSuccess {String} token JWT token for the user
+ * @apiError {String} message Username, email, or password missing
+ * @apiError {String} message Invalid email or password
+ * @apiError {String} message User already exists
+ * @apiError {String} message Error registering user
+ *
+ * @apiDescription This endpoint allows a user to register by providing their username, email, and password. If the credentials are valid,
+ * a JWT token is returned to the user.
  */
 router.route('/user/register').post(
     async (req, res) => {
@@ -78,9 +85,11 @@ router.route('/user/register').post(
  * @apiName Forgot Password
  * @apiGroup User
  * @apiParam {String} email Email of the user
- * @apiSuccess {String} message Password reset link sent successfully
- * @apiError {String} message User not found
- * @apiError {String} message Error sending email
+ * @apiSuccess {String} message Password reset link sent to the user via email
+ * @apiError {String} message Invalid email
+ * @apiError {String} message Error sending password reset link
+ *
+ * @apiDescription This endpoint allows a user to request a password reset link to be sent to their email.
  */
 router.route('/user/forgot-password').post(
     async (req, res) => {
@@ -106,11 +115,13 @@ router.route('/user/forgot-password').post(
  * @apiName Verify Code
  * @apiGroup User
  * @apiParam {String} email Email of the user
- * @apiParam {String} verificationcode Verification code sent to the user via email
- * @apiSuccess {String} message Verification successful
+ * @apiParam {String} verificationcode Verification code provided by the user
+ * @apiSuccess {String} message Verification code is valid
  * @apiSuccess {String} token JWT token for the user
  * @apiError {String} message Invalid verification code
- * @apiError {String} message Error verifying user
+ * @apiError {String} message Error verifying the verification code
+ *
+ * @apiDescription This endpoint verifies the verification code provided by the user.
  */
 router.route('/user/verifyCode').post(
     async (req, res) => {
@@ -121,7 +132,10 @@ router.route('/user/verifyCode').post(
             // 
             const user = await userInteractorPostgres.verifyCode({userForgetPasswordPersistence}, {email, verificationcode});
             // Send the response with the status and user data
-            res.status(user.status).send(user);
+            res.status(user.status).send({
+                message: user.message,
+                token: user.token
+            });
         } catch (err) {
             // Log any errors that occur during the password change process
             console.log(err);
@@ -133,29 +147,32 @@ router.route('/user/verifyCode').post(
 
 /**
  * @api {post} /user/reset-password Reset Password
- * @apiName Reset Password
+ * @apiName ResetPassword
  * @apiGroup User
  * @apiHeader {String} token JWT token for the user
  * @apiParam {String} password New password of the user
- * @apiSuccess {String} message Password reset successfully
+ * @apiSuccess {String} message Password updated successfully
+ * @apiError {String} message Password is required
  * @apiError {String} message Token is missing
  * @apiError {String} message Invalid token
- * @apiError {String} message Error resetting password
+ * @apiError {String} message Error updating password
+ *
+ * @apiDescription This endpoint allows a user to reset their password by providing a valid JWT token and a new password.
  */
 router.route('/user/reset-password').post(
     async (req, res) => {
         // Extract token from request headers
         const token = req.headers['token'];
-        // Destructure newPassword from request body
+        // Destructure new password from request body
         const {password} = req.body;
         
         try {
-            // 
+            // Use userInteractorPostgres to reset the password
             const user = await userInteractorPostgres.resetPassword({userForgetPasswordPersistence}, {token, password});
             // Send the response with the status and user data
             res.status(user.status).send(user);
         } catch (err) {
-            // Log any errors that occur during the password change process
+            // Log any errors that occur during the password reset process
             console.log(err);
             // Rethrow the error to be handled by the caller
             throw err;
@@ -168,26 +185,28 @@ router.route('/user/reset-password').post(
  * @apiName Edit User
  * @apiGroup User
  * @apiHeader {String} token JWT token for the user
- * @apiParam {String} username Username of the user
- * @apiParam {String} email Email of the user
- * @apiParam {String} phonenumber Phonenumber of the user
- * @apiParam {String} profilephoto Profile photo of the user
- * @apiParam {String} phonetoken Phonenumber token of the user
- * @apiSuccess {String} message User updated successfully
+ * @apiParam {String} id Id of the user
+ * @apiParam {String} username New username of the user
+ * @apiParam {String} email New email of the user
+ * @apiParam {String} profilephoto New profile photo of the user
+ * @apiParam {String} phonetoken New phone token of the user
+ * @apiSuccess {String} message User edited successfully
  * @apiError {String} message Token is missing
  * @apiError {String} message Invalid token
- * @apiError {String} message Error updating user
+ * @apiError {String} message Error editing user
+ *
+ * @apiDescription This endpoint allows a user to edit their information in the database.
  */
 router.route('/user/edit').put(
     async (req, res) => {
         // Extract token from request headers
         const token = req.headers['token'];
         // Destructure user details from request body
-        const {username, email, phonenumber, profilephoto, phonetoken} = req.body;
+        const {id, username, email, profilephoto, phonetoken} = req.body;
 
         try {
             // Attempt to edit user details using userInteractorPostgres
-            const user = await userInteractorPostgres.edit({userEditPersistence}, {token, username, email, phonenumber, profilephoto, phonetoken});
+            const user = await userInteractorPostgres.edit({userEditPersistence}, {token, id, username, email, profilephoto, phonetoken});
             // Send the response with the status and user data
             res.status(user.status).send(user);
         } catch (err) {
@@ -199,24 +218,32 @@ router.route('/user/edit').put(
     }
 )
 
+
 /**
  * @api {delete} /user/delete Delete User
- * @apiName Delete User
+ * @apiName DeleteUser
  * @apiGroup User
  * @apiHeader {String} token JWT token for the user
+ * @apiParam {String} id Id of the user to be deleted
  * @apiSuccess {String} message User deleted successfully
- * @apiError {String} message Token is missing
- * @apiError {String} message Invalid token
- * @apiError {String} message Error deleting user
+ * @apiError {String} message Id is required
+ * @apiError {String} message Invalid user ID
+ * @apiError {String} message User not found
+ * @apiError {String} message Error during user deletion
+ *
+ * @apiDescription This endpoint allows a user to be deleted from the database using a JWT token and user ID.
  */
 router.route('/user/delete').delete(
     async (req, res) => {
         // Extract token from request headers
         const token = req.headers['token'];
+        // Destructure user details from request body
+        const {id} = req.body;
         
         try {
             // Attempt to delete the user using userInteractorPostgres
-            const user = await userInteractorPostgres.delete({userDeletePersistence}, {token});
+            const user = await userInteractorPostgres.delete({userDeletePersistence}, {token, id});
+            res.status(user.status).send(user);
             // Send the response with the status and user data
             res.status(user.status).send(user)
         } catch (err) {
